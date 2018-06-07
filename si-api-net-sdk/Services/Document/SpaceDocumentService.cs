6540c433dcb8a26bf8a73adf3bc1f26bcd1fd1cd
David404 <david.j@studio404.si>
2018-06-07 10:52:38 +0200

    remake documentservice

--- si-api-net-sdk/Services/Document/SpaceDocumentService.cs
+++ si-api-net-sdk/Services/Document/SpaceDocumentService.cs
@@ -6,17 +6,12 @@ namespace SpaceInvoices
 {
     public class SpaceDocumentService : SpaceService
     {
-
         public SpaceDocumentService() : base(null) { }
         public SpaceDocumentService(string apiKey) : base(apiKey) { }
 
         //Sync
         public virtual SpaceDocument Create(string organizationId, SpaceDocumentCreateOptions document)
         {
-            //return Mapper<SpaceDocument>.MapFromJson(
-            //    Requestor.PostString(this.ApplyAllParameters(document, $"{Urls.Organizations}/{organizationId}/documents", false))
-            //);
-
             return Mapper<SpaceDocument>.MapFromJson(
                 Requestor.Post(document, $"{Urls.Organizations}/{organizationId}/documents")
             );
@@ -24,32 +19,11 @@ namespace SpaceInvoices
 
         public virtual List<SpaceDocument> List(string organizationId, string filter)
         {
-            //   return Mapper<SpaceDocument>.MapCollectionFromJson(
-            //       Requestor.Get($"{Urls.Organizations}/{organizationId}/documents")
-            //);
-
-            var filtr = @"{
-                              filter: {
-                                where: {
-                                  type: 'invoice'
-                                }
-                                }
-                              }";
-
-       //     return Mapper<SpaceDocument>.MapCollectionFromJson(
-       //         Requestor.GetFilter($"{Urls.Organizations}/{organizationId}/documents",filtr)
-       //);
-
             return Mapper<SpaceDocument>.MapCollectionFromJson(
-                Requestor.GetFilter($"{Urls.Organizations}/{organizationId}/documents", filtr)
-       );
-        }
-
-        public virtual SpaceDocument GetById(string documentId)
-        {
-            return Mapper<SpaceDocument>.MapFromJson(
-                Requestor.Get($"{Urls.Documents}/{documentId}")
+                Requestor.GetFilter($"{Urls.Organizations}/{organizationId}/documents", filter)
             );
         }
+
+
     }
 }
